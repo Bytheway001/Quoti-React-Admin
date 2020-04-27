2b2920ec5038e53756fa38f0c932ff3b4ee800a9
Rafael <Bytheway0708@gmail.com>
2020-04-27 16:58:06 -0400

    Updates

---
+++ precache-manifest.dbdf20a129dc9c51396e3e2082e646c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b15736b43c34837fad18af7a775fd89e",
+    "url": "/Quoti-React-Admin/index.html"
+  },
+  {
+    "revision": "a1f4478bcb53243c704d",
+    "url": "/Quoti-React-Admin/static/css/main.d3323dc9.chunk.css"
+  },
+  {
+    "revision": "fa7af33853fc47e5b8ee",
+    "url": "/Quoti-React-Admin/static/js/2.42149d3e.chunk.js"
+  },
+  {
+    "revision": "a1f4478bcb53243c704d",
+    "url": "/Quoti-React-Admin/static/js/main.ccf0c1f3.chunk.js"
+  },
+  {
+    "revision": "d1f7aec66c6d8a907476",
+    "url": "/Quoti-React-Admin/static/js/runtime-main.fb09f217.js"
+  }
+]);
